5fc365658cf26400c3b1ad36f43816337ae56df9
Katniss-Nataliia <natko.boiko@gmail.com>
2023-10-02 21:21:14 -0600

    added the code

---
+++ contacts.js
@@ -0,0 +1,73 @@
+const fs = require('fs');
+const path = require('path');
+
+const contactsPath = path.join(__dirname, 'db', 'contacts.json');
+
+function listContacts() {
+    fs.readFile(contactsPath, 'utf-8', (err, data) => {
+        if(err){
+            console.error(err);
+            return
+        }
+        const contacts = JSON.parse(data);
+        console.log(contacts)
+    })
+  }
+  
+  function getContactById(contactId) {
+    fs.readFile(contactsPath, 'utf-8', (err, data)=>{
+        if(err){
+            console.err(err);
+            return
+        }
+        const contacts = JSON.parse(data);
+        const contact = contacts.find(c=>c.id === contactId);
+
+        if(contact){
+            console.table([contact]);
+        }else{
+            console.log(`Contact with this id: ${contactId} does not exist`);
+        }
+    })
+  }
+  
+  function removeContact(contactId) {
+    fs.readFile(contactsPath, 'utf-8', (err, data)=>{
+        if (err){
+            console.err(err);
+            return;
+        }
+        const contacts = JSON.parse(data);
+        const contact = contacts.find(c=>c.id === contactId);     
+        
+        if (contact){
+            delete contact;
+            console.log(contact)
+        }else{
+            console.log(`Contact with this id: ${contactId} not found`);
+        }
+        
+    })
+  }
+  
+  function addContact(name, email, phone) {
+    fs.readFile(contactsPath, 'utf-8', (err, data)=>{
+        if(err){
+            console.err(err);
+            return;
+        }
+        const contacts = JSON.parse(data);
+        const newContact = {name, email, phone};
+
+        contacts.push(newContact);
+        console.log(contacts)
+    })
+  }
+
+module.exports = {
+    listContacts,
+    getContactById,
+    removeContact,
+    addContact
+
+}
